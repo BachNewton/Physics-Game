2cc31e3f71dbb38876163c594bbc7ebb02f09425
BachNewton <kyle1235@gmail.com>
2019-02-08 13:02:45 -0500

    Removed some Cannon world setup, makes code cleaner

--- Resources/JavaScript/main.js
+++ Resources/JavaScript/main.js
@@ -19,36 +19,10 @@ function initCannon() {
     // Setup our world
     world = new CANNON.World();
 
-    // world.quatNormalizeSkip = 0;
-    // world.quatNormalizeFast = false;
-
     var solver = new CANNON.GSSolver();
-
-    // world.defaultContactMaterial.contactEquationStiffness = 1e9;
-    // world.defaultContactMaterial.contactEquationRelaxation = 4;
-
-    // solver.iterations = 7;
-    // solver.tolerance = 0.1;
-
-    var split = true;
-    if (split)
-        world.solver = new CANNON.SplitSolver(solver);
-    else
-        world.solver = solver;
+    world.solver = new CANNON.SplitSolver(solver);
 
     world.gravity.set(0, -10, 0);
-
-    // world.broadphase = new CANNON.NaiveBroadphase();
-
-    // Create a slippery material (friction coefficient = 0.0)
-    var physicsMaterial = new CANNON.Material("slipperyMaterial");
-    var physicsContactMaterial = new CANNON.ContactMaterial(physicsMaterial,
-        physicsMaterial,
-        0.0, // friction coefficient
-        0.3  // restitution
-    );
-    // We must add the contact materials to the world
-    world.addContactMaterial(physicsContactMaterial);
 
     // Create a player
     var mass = 5, radius = 1.3;
